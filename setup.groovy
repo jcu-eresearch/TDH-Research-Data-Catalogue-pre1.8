15921486ad685f6cd2bce1f4b59ebbe5bfd5f191
NigelB <nigel.blair@gmail.com>
2012-09-12 09:13:29 +1000

    Updated deployment location

--- setup.groovy
+++ setup.groovy
@@ -1,7 +1,7 @@
 String projectHome = project.properties["project.home"];
 if (projectHome == null) {
     String userHome = System.getProperty("user.home");
-    File redboxHome = new File(new File(userHome, "deployment"), "jcu-redbox");
+    File redboxHome = new File(new File(userHome, "deployment"), "redbox");
     project.properties["project.home"] = redboxHome.absolutePath;
     new File(redboxHome, "system").mkdirs();
 }

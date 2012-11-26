b97aa334b3ce0c716b76c07d71a282780a3825b3
NigelB <nigel.blair@gmail.com>
2012-11-26 11:13:47 +1000

    Added License to python code

--- src/main/python/bin/process_rif_cs.py
+++ src/main/python/bin/process_rif_cs.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+#JCU RIF-CS Pre-Processor
+#Copyright (C) 2012  Nigel Bajema, James Cook University
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License along
+#with this program; if not, write to the Free Software Foundation, Inc.,
+#51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 import sys, glob, os.path
 
@@ -94,6 +110,10 @@ if __name__ == "__main__":
             output = args.output[0]
         if args.inplace:
             output = None
+
+        config = {
+            "redbox_host"
+        }
 
         for fl in args.file:
             for _file in glob.glob(fl):
--- src/main/python/jcu/__init__.py
+++ src/main/python/jcu/__init__.py
@@ -1,3 +1,20 @@
+#JCU RIF-CS Pre-Processor
+#Copyright (C) 2012  Nigel Bajema, James Cook University
+#
+#This program is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License along
+#with this program; if not, write to the Free Software Foundation, Inc.,
+#51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 from java.io import PrintWriter
 
 class log:
@@ -31,3 +48,28 @@ class config:
         if d is None:
             return None
         return d.replace("${fascinator.home}",self.home)
+
+
+class config_helper:
+
+    def __init__(self, config):
+        self.config = config
+
+    def __setattr__(self, key, value):
+        if key == "config":
+            self.__dict__[key] = value
+        else:
+            self.config[key] = value
+
+    def __getitem__(self, item):
+        return self.config[item]
+
+    def __getattr__(self, key):
+        if key is "__str__": return self.config.__str__
+        elif key is "__repr__": return self.config.__repr__
+        elif key is "__iter__": return self.config.__iter__
+        elif key is "config_delegate": return self.config
+        return self.config[key]
+
+    def __contains__(self, item):
+        return self.__getattr__(item)

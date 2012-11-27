4dd1a321d32d08577766246988bbea9d1b243750
NigelB <nigel.blair@gmail.com>
2012-11-27 13:37:47 +1000

    Remove alerts stuff

--- src/main/python/bin/process_rif_cs.py
+++ src/main/python/bin/process_rif_cs.py
@@ -34,10 +34,6 @@ if __name__ == "__main__":
     argParse = ArgumentParser(description="Process the JCU RIF-CS imports")
     argParse.add_argument('-i','--inplace', dest='inplace', action='store_true', default=False, help='Update the files in place')
     argParse.add_argument('-o','--output', dest='output', action='store',  help='The output location, mutually exclusive with the inplace option.')
-    argParse.add_argument('-p','--redbox-path', dest='path', nargs=1, action='store',  help='The base path of the ReDBoX Install')
-    argParse.add_argument('-a','--alert', dest='alerts', action='store_true', default=False, help='Post-process with ReDBoX alert script.')
-    argParse.add_argument('-m','--rifcs-map-file', nargs=1, dest='rmap', action='store', default=None, help='The location of the rifcs map file.')
-    argParse.add_argument('-x','--xml-map-file', nargs=1, dest='xmap', action='store', default=None, help='The location of the xml map file.')
     argParse.add_argument('--redbox-protocol', nargs=1, dest='redbox_protocol', action='store', default="http", help='The protocol that redbox is running on, default: http.')
     argParse.add_argument('--redbox-host', nargs=1, dest='redbox_host', action='store', default="localhost", help='The hostname that redbox is running on, default: localhost.')
     argParse.add_argument('--redbox-port', nargs=1, dest='redbox_port', action='store', default=9000, help='The port that redbox is running on, default: 9000.')
@@ -55,57 +51,14 @@ if __name__ == "__main__":
         print "Options --inplace and --output cannot be used at the same time."
         argParse.print_help()
         sys.exit(5)
-    print args.path, args.file
+    output = None
+    if args.output is not None:
+        output = args.output
 
-    if args.path is not None and os.path.exists(args.path[0]):
-        fascinator_home = os.path.join(args.path[0], "home")
+    for fl in args.file:
+        for _file in glob.glob(fl):
+            print "Processing: ", _file
+            fix(_file, args, output)
 
-        FascinatorHome.setPath(fascinator_home)
-        import json
-        system = open(os.path.join(fascinator_home, "system-config.json"))
-        system_data = json.load(system)
-        system.close()
 
-        _config = config(fascinator_home, system_data)
-        output = _config.getString(None, ["alerts", "path"])
-
-        if args.rmap is not None:
-            system_data["alerts"]["xmlMaps"]["rif"] = args.rmap[0]
-        if args.xmap is not None:
-            system_data["alerts"]["xmlMaps"]["xml"] = args.xmap[0]
-
-        if args.output is not None:
-            output = args.output
-        if args.inplace:
-            output = None
-        for fl in args.file:
-            for _file in glob.glob(fl):
-                print "Processing: ", _file
-                fix(_file, args, output)
-
-        if args.alerts:
-            import csv
-            from com.googlecode.fascinator import HarvestClient
-            from com.googlecode.fascinator.common import FascinatorHome
-            from com.googlecode.fascinator.common import JsonObject
-            from com.googlecode.fascinator.common import JsonSimple
-
-            from java.io import File
-            from java.io import FileInputStream
-            from java.io import InputStreamReader
-            from java.lang import Exception
-
-            from org.dom4j import DocumentFactory
-            from org.dom4j.io import SAXReader
-            from org.json.simple import JSONArray
-            from org.dom4j.xpath import DefaultNamespaceContext
-            from org.dom4j.xpath import DefaultXPath
-            from org.jaxen import SimpleNamespaceContext
-            import csv
-            import os
-            import shutil
-            import shutil
-            import time
-            alerts = AlertsData()
-            alerts.__activate__({"log":log(), 'systemConfig': _config, 'response': response()})
 

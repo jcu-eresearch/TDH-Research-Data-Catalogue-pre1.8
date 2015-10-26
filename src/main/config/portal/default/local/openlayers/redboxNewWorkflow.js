858c13963eeba2fa95f22caac7ca9256587fd7a9
Jay van Schyndel <ozej8y@gmail.com>
2015-10-26 11:59:38 +1000

    Issue 19: altered to use https for Mapquest

--- src/main/config/portal/default/local/openlayers/redboxNewWorkflow.js
+++ src/main/config/portal/default/local/openlayers/redboxNewWorkflow.js
@@ -242,16 +242,18 @@ function loadOpenLayers() {
             gLayer = new OpenLayers.Layer.Google("Google", {sphericalMercator:true});
         }
         if  (layers == "mapquest"){
-            osmLayer = new OpenLayers.Layer.XYZ("OpenStreetMap", ["http://otile1.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.png", "http://otile2.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.png", 
-                                                                  "http://otile3.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.png", "http://otile4.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.png"], 
-                                                                 {attribution: "Data, imagery and map information provided by <a href='http://www.mapquest.com/'  target='_blank'>MapQuest</a>, <a href='http://www.openstreetmap.org/' target='_blank'>Open Street Map</a> and contributors, <a href='http://creativecommons.org/licenses/by-sa/2.0/' target='_blank'>CC-BY-SA</a>  <img src='http://developer.mapquest.com/content/osm/mq_logo.png' border='0'>", 
+            osmLayer = new OpenLayers.Layer.XYZ("OpenStreetMap", ["https://otile1-s.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.png",
+                                                                  "https://otile2-s.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.png", 
+                                                                  "https://otile3-s.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.png",
+                                                                  "https://otile4-s.mqcdn.com/tiles/1.0.0/map/${z}/${x}/${y}.png"], 
+                                                                 {attribution: "Data, imagery and map information provided by <a href='http://www.mapquest.com/'  target='_blank'>MapQuest</a>, <a href='http://www.openstreetmap.org/' target='_blank'>Open Street Map</a> and contributors, <a href='http://creativecommons.org/licenses/by-sa/2.0/' target='_blank'>CC-BY-SA</a>  <img src='https://developer.mapquest.com/content/osm/mq_logo.png' border='0'>", 
                                                                   transitionEffect: "resize",
                                                                   wrapDateLine: true});
-            imageryLayer = new OpenLayers.Layer.XYZ("Imagery", ["http://otile1.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.png",
-                                                                "http://otile2.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.png",
-                                                                "http://otile3.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.png",
-                                                                "http://otile4.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.png"],
-                                                                {attribution: "Tiles Courtesy of <a href='http://open.mapquest.co.uk/' target='_blank'>MapQuest</a>. Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency. <img src='http://developer.mapquest.com/content/osm/mq_logo.png' border='0'>",
+            imageryLayer = new OpenLayers.Layer.XYZ("Imagery", ["https://otile1-s.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.png",
+                                                                "https://otile2-s.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.png",
+                                                                "https://otile3-s.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.png",
+                                                                "https://otile4-s.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.png"],
+                                                                {attribution: "Tiles Courtesy of <a href='http://open.mapquest.co.uk/' target='_blank'>MapQuest</a>. Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency. <img src='https://developer.mapquest.com/content/osm/mq_logo.png' border='0'>",
                                                                  transitionEffect: "resize",
                                                                  wrapDateLine: true});
         }
